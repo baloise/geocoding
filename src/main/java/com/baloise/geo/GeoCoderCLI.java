ce028763cabf0aabab896c18fc9645bdd4178ae8
Matthias Cullmann <matthias.cullmann@baloise.ch>
2017-05-09 10:59:20 +0200

    kryo for faster loading

--- src/main/java/com/baloise/geo/GeoCoderCLI.java
+++ src/main/java/com/baloise/geo/GeoCoderCLI.java
@@ -1,5 +1,13 @@
 package com.baloise.geo;
 
+import static java.lang.String.format;
+import static java.lang.System.currentTimeMillis;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -8,21 +16,50 @@ import com.baloise.geo.model.Jvnfras;
 import com.baloise.geo.model.NewGeb;
 import com.baloise.geo.model.NewPlz1;
 import com.baloise.geo.model.NewStr;
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
 
 public class GeoCoderCLI {
 
-	public GeoCoderCLI() {
-	}
+	Jvnfras<Integer, NewPlz1> plz = new Jvnfras<>();
+	Jvnfras<String, NewStr> strassen = new Jvnfras<>();
+	Jvnfras<String, NewGeb> gebaeude = new Jvnfras<>();
 
 	public static void main(String[] args) throws Exception {
-		Jvnfras<Integer, NewPlz1> plz = new Jvnfras<>();
-		Jvnfras<String, NewStr> strassen = new Jvnfras<>();
-		Jvnfras<String, NewGeb> gebaeude = new Jvnfras<>();
-		
-		Files.lines(
-				Paths.get("Post_Adressdaten20170425.csv"),
-				Charset.forName("ISO-8859-1")
-		).forEach((line) -> {
+		Kryo kryo = new Kryo();
+		File save = new File("save.bin");
+		GeoCoderCLI geo = load(kryo, save);
+
+		System.out.println(geo.plz.size());
+		System.out.println(geo.strassen.size());
+		System.out.println(geo.gebaeude.size());
+
+		if (!save.exists()) {
+			try (Output output = new Output(new FileOutputStream(save));) {
+				kryo.writeObject(output, geo);
+			}
+		}
+
+	}
+
+	private static GeoCoderCLI load(Kryo kryo, File save) throws FileNotFoundException, IOException {
+		GeoCoderCLI geo;
+		long start = currentTimeMillis();
+		if (save.exists()) {
+			try (Input input = new Input(new FileInputStream(save))) {
+				geo = kryo.readObject(input, GeoCoderCLI.class);
+			}
+		} else {
+			geo = new GeoCoderCLI();
+			geo.parseCSV();
+		}
+		System.out.println(format("loaded in %s msec", currentTimeMillis() - start));
+		return geo;
+	}
+
+	private void parseCSV() throws IOException {
+		Files.lines(Paths.get("Post_Adressdaten20170425.csv"), Charset.forName("ISO-8859-1")).forEach((line) -> {
 			String[] tokens = line.split(";");
 			int REC_ART = Integer.valueOf(tokens[0]);
 			switch (REC_ART) {
@@ -38,15 +75,9 @@ public class GeoCoderCLI {
 			default:
 				break;
 			}
-			
+
 		});
-		
-		System.out.println(plz.size());
-		System.out.println(strassen.size());
-		System.out.println(gebaeude.size());
-		
+
 	}
-	
-	
 
 }
